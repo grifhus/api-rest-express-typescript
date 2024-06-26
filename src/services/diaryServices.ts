da81b9248cf668f8355023e06664f9b7a57fd1e0
David Julian Jaramillo <davidjulianjaramillo@gmail.com>
2024-06-25 21:43:26 -0500

    Corregir manejo de posibles valores undefined en findById

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -2,28 +2,32 @@ import { DiaryEntry, NonSensitiveInfoDiaryEntry } from "../types"
 import diaryData from "./diaries.json"
 
 const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
-// Realizamos una aserción de tipo para garantizar que diaryData se trate como un Array<DiaryEntry>, asegurando que los datos cumplen con la estructura esperada.
 
+// Función para obtener todas las entradas
 export const getEntries = (): DiaryEntry[] => diaries
 
+// Función para encontrar una entrada por ID sin la información sensible
 export const findById = (id: number): NonSensitiveInfoDiaryEntry | undefined => {
-    const entry = diaries.find((d) => d.id === id);
-  
-    if (entry !== null ) { 
-      const { comment, ...restOfDiary } = entry;
-      return restOfDiary;
-    }
-    return undefined;
-  };
-export const getEntriesWithoutSensitive = (): NonSensitiveInfoDiaryEntry[] => {
-  return diaries.map(({ id, date, weather, visibility }) => {
-    return {
-      id,
-      date,
-      weather,
-      visibility,
-    }
-  })
+  const entry = diaries.find((d) => d.id === id);
+
+  if (entry) {
+    // Desestructuramos para quitar la propiedad 'comment'
+    const { comment, ...restOfDiary } = entry;
+    return restOfDiary;
+  }
+
+  return undefined;
 }
 
+// Función para obtener todas las entradas sin la información sensible
+export const getEntriesWithoutSensitive = (): NonSensitiveInfoDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => ({
+    id,
+    date,
+    weather,
+    visibility,
+  }))
+}
+
+// Función de marcador para agregar una entrada
 export const addEntry = (): undefined => undefined
